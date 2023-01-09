748a844506a13960d67448af91c164b9c32245c6
ZrllXz4 <zrill4seven@gmail.com>
2023-01-09 18:32:01 +0700

    xcx

---
+++ plugins/downloader-tiktok.js
@@ -0,0 +1,22 @@
+import { tiktokdl, tiktokdlv2, tiktokdlv3 } from '@bochilteam/scraper'
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    if (!args[0]) throw `Input URL! contoh : ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
+
+  const { author: { nickname }, video, description } = await tiktokdl(args[0])
+        .catch(async _ => await tiktokdlv2(args[0]))
+        .catch(async _ => await tiktokdlv3(args[0]))
+    const url = video.no_watermark2 || video.no_watermark || 'https://tikcdn.net' + video.no_watermark_raw || video.no_watermark_hd
+    if (!url) throw 'Can\'t download video!'
+    conn.sendHydrated(m.chat, `▷${htki} 𝙽𝚒𝚑 𝙺𝚊𝚔 ${htka}◁`, `⌕ ɴɪᴄᴋɴᴀᴍᴇ : ${nickname}${description ? `\n⌕ ᴅᴇsᴄʀɪᴘᴛɪᴏɴ : \n${description}` : ''}`, await (await fetch(url)).buffer(),
+        url, '🌎 s ᴏ ᴜ ʀ ᴄ ᴇ', null, null, [
+        ['ᴅᴏɴᴀꜱɪ', `.donasi`],
+        ['ᴀᴜᴅɪᴏ', `.tiktokaudio ${args}`],
+        [null, null]], m)
+}
+handler.help = ['tiktok', 'tiktokdl'].map(v => v + ' <url>')
+handler.tags = ['downloader']
+handler.command = /^(tik(tok)?(tok)?(dl)?)$/i
+
+export default handler
